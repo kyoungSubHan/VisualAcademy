b11d3ecd7ab698cd253ace939723d7127e1b3818
kyoungSubHan <ost970806@nate.com>
2020-04-02 21:04:18 +0900

    추가 여러가지

---
+++ VisualAcademy/Pages/Portfolios/PortfolioWebsite.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VisualAcademy.Pages.Portfolios
+{
+    public partial class PortfolioWebsite
+    {
+        [Inject]
+        public NavigationManager NavigationManagerRef { get; set; }
+        protected override void OnInitialized()
+        {
+            
+        }
+
+        protected void GotoDotNetKorea()
+        {
+            NavigationManagerRef.NavigateTo("/");
+        }
+    }
+}
